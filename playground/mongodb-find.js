5d727f9935420c4aa4e44c4f6bf5a61bd7bc1a6e
April Copes <antdevelopment1@gmail.com>
2018-11-08 20:19:41 -0500

    Adds query to get all documents from our todos collection

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -9,6 +9,14 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to server.');
     const db = client.db('TodoApp');
 
+    // Fetches all todos from Todos collection
+    db.collection('Todos').find().toArray()
+        .then( (docs) => {
+            console.log('Todos');
+            console.log(JSON.stringify(docs, undefined, 2));
+        }, (err) => {
+            console.log('Unable to fetch todos', err);
+    });
     
-    client.close();
+    // client.close();
 });
