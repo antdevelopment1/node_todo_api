a884cc53252469c216822398d7e40263a765d071
April Copes <antdevelopment1@gmail.com>
2018-11-08 21:53:11 -0500

    Updates completed boolean in users table to true

---
+++ playground/mongodb-update.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    { useNewUrlParser: true }
+    if (err) {
+       return  console.log('Unable to connect to database server');
+    }
+    console.log('Connected to server.');
+    const db = client.db('TodoApp');
+
+    
+    db.collection('Todos').findOneAndUpdate({
+        _id: new ObjectID('5be4deffe4b9df8045a3208b')
+    }, {
+       $set: {
+           completed: true
+       } 
+    }, {
+        returnOriginal: false
+
+    }).then(result => {
+        console.log(result);
+    })
+    // client.close();
+});
