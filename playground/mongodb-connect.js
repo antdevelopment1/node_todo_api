a889c738653dee7f324ceb1e6f88acf37ec52493
April Copes <antdevelopment1@gmail.com>
2018-11-08 20:07:52 -0500

    uses destructuring to access mongodb properties

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,5 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    { useNewUrlParser: true }
     if (err) {
        return  console.log('Unable to connect to database server');
     }
@@ -15,15 +18,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     //     } console.log(JSON.stringify(result.ops, undefined, 2))
     // })
 
-    db.collection('Users').insertOne({
-        name: 'Lauren Wilkerson',
-        age: 27,
-        location: 'Next to me'
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert user');
-        } console.log(JSON.stringify(result.ops, undefined, 2))
-    })
+    // db.collection('Users').insertOne({
+    //     name: 'Lauren Wilkerson',
+    //     age: 27,
+    //     location: 'Next to me'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert user');
+    //     } console.log(result.ops[0]._id.getTimestamp())
+    // })
 
     // Insert new doc into users (name, age, locationString) 
     client.close();
