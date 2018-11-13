434fba93f80514af067650416b4cc5c981082e87
April Copes <antdevelopment1@gmail.com>
2018-11-12 21:36:11 -0500

    Adds validator to text object

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 let Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        // Adds validators
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
         type: Boolean
@@ -19,20 +23,19 @@ let Todo = mongoose.model('Todo', {
 // let newTodo = new Todo({
 //     text: 'Cook dinner'
 // })
-
-let secondTodo = new Todo({
-    text: 'Eat Ice Cream',
-    completed: true,
-    completedAt: 20
-})
-
-// Saves new info to mongo database and save also returns a promise
 // newTodo.save()
 //     .then((doc) => {
 //         console.log('Saved Todo', doc);
 //     }, (e) => {
 //         console.log('Unable to save todo');
 //     })
+
+
+let secondTodo = new Todo({
+    text: '    Edit this video     '
+})
+
+// Saves new info to mongo database and save also returns a promise
 
 secondTodo.save()
     .then((doc) => {
