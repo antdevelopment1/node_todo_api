82ace49d561c36e486dec2cdb2787dca0b2a8974
April Copes <antdevelopment1@gmail.com>
2018-11-12 23:55:44 -0500

    test error

--- server/models/user.js
+++ server/models/user.js
@@ -10,15 +10,7 @@ let User = mongoose.model('User', {
     }
 });
 
-let user1 = new User({
-    email: 'antdevelopment111@gmail.com   '
-});
-user1.save()
-    .then( (user) => {
-        console.log('Saved user', user)
-    }, (error) => {
-        console.log('Unable to save user', error);
-    })
+
 
 
 module.exports = {User};
