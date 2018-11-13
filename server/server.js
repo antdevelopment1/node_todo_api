864579fcfac30606285218151ae54c97e63207b9
April Copes <antdevelopment1@gmail.com>
2018-11-12 23:56:00 -0500

    error test

--- server/server.js
+++ server/server.js
@@ -1,6 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
+const {Todos} = require('./models/todos');
 const {User} = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+
+
+app.post('/todos', (req, res) => {
+    // This body gets stored by body parser
+    let todos = new Todos({
+        text: req.body.text
+    });
+
+    console.log(req.body);
+    
+    todos.save()
+        .then( (doc) => {
+            res.send(doc);
+        }, (e) => {
+            res.status(400).send(e);
+        })
+})
+
+
+// Get 
+
+
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+})
 
 
 
