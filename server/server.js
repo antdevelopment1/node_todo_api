d7345a2e8ecae130388652ce2c4d40f1756848a1
April Copes <antdevelopment1@gmail.com>
2018-11-12 22:29:57 -0500

    Refactors server js to have access to database amd models layer

--- server/server.js
+++ server/server.js
@@ -1,68 +1,9 @@
-let mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-
-let Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        // Adds validators
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
-});
-// let newTodo = new Todo({
-//     text: 'Cook dinner'
-// })
-// newTodo.save()
-//     .then((doc) => {
-//         console.log('Saved Todo', doc);
-//     }, (e) => {
-//         console.log('Unable to save todo');
-//     })
-
-
-let secondTodo = new Todo({
-    text: '    Edit this video     '
-})
-
-// Saves new info to mongo database and save also returns a promise
-
-secondTodo.save()
-    .then((doc) => {
-    console.log('Saved Todo', doc);
-}, (e) => {
-    console.log('Unable to save todo');
-})
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 
 
-let User = mongoose.model('User', {
-    email: {
-        type: String,
-        // Adds validators
-        required: true,
-        minlength: 1,
-        trim: true
-    }
-})
 
-let user1 = new User({
-    email: 'antdevelopment111@gmail.com   ',
-})
-user1.save()
-    .then( (user) => {
-        console.log('Saved user', user)
-    }, (error) => {
-        console.log('Unable to save user');
-    })
+
+
